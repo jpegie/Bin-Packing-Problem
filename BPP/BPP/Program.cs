4a51204c62160567680a110db4586a29cdca56e1
jpegie <polygalov.vladimir@gmail.com>
2021-12-04 22:40:48 +0500

    Добавляю алгоритмы перебора, FF, FFS

--- BPP/BPP/Program.cs
+++ BPP/BPP/Program.cs
@@ -26,26 +26,50 @@ namespace BPP
         }
         class Bin
         {
-            int m;
+            int total_capacity = 0;
+            double current_capacity = 0;
+            
             List<Item> items = new List<Item>();
-            public Bin(int m)
+            public Bin(int total_capacity)
             {
-                this.m = m;
+                this.total_capacity = total_capacity;
             }
             public List<Item> Items
             {
                 get { return items; }
             }
+            public bool AddItem(Item item)
+            {
+                if(FreeCapacity() < item.Weight)
+                    return false;
+                items.Add(item);
+                current_capacity += item.Weight;
+                return true;
+            }
+            /*public int TotalCapacity
+            {
+                get { return total_capacity; }
+            }*/
+
+            /*public double CurrentCapacity()
+            {
+                return current_capacity;
+            }*/
+            private double FreeCapacity()
+            {
+                return total_capacity - current_capacity;
+            }
         }
         class Packing
         {
+            List<List<Item>> items_combs = new List<List<Item>>();
             List<Item> items = new List<Item>();
             List<Bin> bins = new List<Bin>();
-            int bin_size = 0;
-            public Packing(List<Item>items, int bin_size)
+            int bin_capacity = 0;
+            public Packing(List<Item> items, int bin_capacity)
             {
                 this.items = items;
-                this.bin_size = bin_size;
+                this.bin_capacity = bin_capacity;
             }
 
             //перебор всех возможных n! комбинаций предметов с целью поиска лучшей для умещения в меньшее кол-во контейнеров
@@ -56,51 +80,109 @@ namespace BPP
                 //применить FF
                 //сохранить информацию о заполненных контейнерах и саму i-ую комбинацию
                 //найти лучшую комбинацию
-                return bins;
+
+                int best_bin = 0;
+
+                List<Bin> best_bin_pack = new List<Bin>();
+                List<List<Bin>> bins_combs = new List<List<Bin>>();
+
+                PermuteItems(items, 0);
+
+                for(int i=0;i<items_combs.Count ;++i)
+                {
+                    items = items_combs[i];
+                    bins.Clear();
+                    FF();
+                    bins_combs.Add(bins);
+                }
+                for (int i = 1; i < bins_combs.Count; ++i)
+                    if (bins_combs[i].Count < bins_combs[best_bin].Count)
+                        best_bin = i;
+                return bins_combs[best_bin];
+            }
+            private void PermuteItems(List<Item> perm, int start)
+            {
+
+                if (start == items.Count)
+                {
+                    items_combs.Add(new List<Item>(perm));
+                }
+                else
+                {
+                    for (int j = start; j < items.Count; ++j)
+                    {
+                        SwapItems(ref perm, start, j);
+                        PermuteItems(perm, start + 1);
+                        SwapItems(ref perm, start, j);
+                    }
+                }
+            }
+            private void SwapItems(ref List<Item> items_list, int item1_i, int item2_i)
+            {
+                Item buff = items_list[item1_i];
+                items_list[item1_i] = items_list[item2_i];
+                items_list[item2_i] = buff;
             }
             //"первый подходящий"
             public List<Bin> FF()
             {
+                bins.Add(new Bin(bin_capacity)); //добавляем первый контейнер
+                bool found_fit_bin = false;
+                for(int i=0;i<items.Count ; ++i)
+                {
+                    found_fit_bin = false;
+                    //проход по всем контейнерам с целью поиска первого подходящего
+                    for (int j=0; j<bins.Count && !found_fit_bin; ++j) 
+                    {
+                        found_fit_bin = bins[j].AddItem(items[i]);
+                    }
+                    //если подхожящего контейнера не найдено, то добавим новый контейнер
+                    if (!found_fit_bin)
+                    {
+                        bins.Add(new Bin(bin_capacity));
+                        bins[bins.Count - 1].AddItem(items[i]);
+                    }
+
+                }
                 return bins;
             }
 
             //"первый подходящий" только в отсортированном списке предметов
             public List<Bin> FFS()
             {
-                //отсортировать
-                //FF()
-                return bins;
+                items.Sort((x, y) => x.Weight.CompareTo(y.Weight));
+                return FF();
             }
         }
 
         void TestGenerator()
         {
-            
+
         }
 
         static void Main()
         {
-            List<Bin> bins = new List<Bin>();       int bin_size = 0;
+            List<Bin> bins = new List<Bin>();       int bin_capacity = 0;
             List<Item> items = new List<Item>();    int items_amnt = 0;
             Packing packing; 
 
-            Console.WriteLine("Кол-во предметов: "); int.TryParse(Console.ReadLine(), out items_amnt);
-            Console.WriteLine("Размерность контейнера: "); int.TryParse(Console.ReadLine(), out bin_size);
-
-            for(int i=0;i<items_amnt ; ++i)
+            Console.Write("Кол-во предметов: "); int.TryParse(Console.ReadLine(), out items_amnt);
+            
+            for (int i = 0; i < items_amnt; ++i)
             {
                 double weight = 0;
                 Console.Write($"Вес {i + 1} предмета: "); double.TryParse(Console.ReadLine(), out weight);
-                items.Add(new Item(i,weight));
+                items.Add(new Item(i, weight));
             }
 
+            Console.Write("Размерность контейнера: "); int.TryParse(Console.ReadLine(), out bin_capacity);
             Console.Write(
-                        "Выберите тип упаковки: " + 
-                        "1. Перебор всех возможных комбинаций предметов для нахождеия лучшего случая" + 
-                        "2. FF" + 
-                        "3. BF"
+                        "Выберите тип упаковки:\n" + 
+                        "1. Перебор всех возможных комбинаций предметов для нахождеия лучшего случая\n" + 
+                        "2. FF\n" + 
+                        "3. FFS\n"
                          );
-            packing = new Packing(items, bin_size);
+            packing = new Packing(items, bin_capacity);
             int pack_id; int.TryParse(Console.ReadLine(), out pack_id);
             switch (pack_id)
             {
@@ -121,14 +203,14 @@ namespace BPP
                 }
             }
 
-            /*Console.WriteLine($"Кол-во использованных контейнеров: {bins.Count}");
-            for(int i=0;i<bins.Count; ++i)
+            Console.WriteLine($"Кол-во использованных контейнеров: {bins.Count}");
+            for (int i = 0; i < bins.Count; ++i)
             {
                 Console.WriteLine($"Предметы контейнера {i + 1}: ");
-                foreach(Item item in bins[i].Items)
+                foreach (Item item in bins[i].Items)
                     Console.WriteLine($"[{item.Index}] : {item.Weight}");
                 Console.WriteLine(new String('-', 25));
-            }*/
+            }
         }
     }
 }
